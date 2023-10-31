812f94da24f5176f33615c89a372fc5f7fd45f04
parsa <parsa@beastbrain.org>
2023-10-31 18:01:41 +0330

    bug

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,31 +1,9 @@
-#!/bin/sh
-set -e
-
-# no arguments passed
-# or first arg is `-f` or `--some-option`
-if [ "$#" -eq 0 ] || [ "${1#-}" != "$1" ]; then
-	# add our default arguments
-	set -- dockerd \
-		--host=unix:///var/run/docker.sock \
-		--host=tcp://0.0.0.0:2375 \
-		"$@"
-fi
-
-if [ "$1" = 'dockerd' ]; then
-	if [ -x '/usr/local/bin/dind' ]; then
-		# if we have the (mostly defunct now) Docker-in-Docker wrapper script, use it
-		set -- '/usr/local/bin/dind' "$@"
-	fi
-
-	# explicitly remove Docker's default PID file to ensure that it can start properly if it was stopped uncleanly (and thus didn't clean up the PID file)
-	find /run /var/run -iname 'docker*.pid' -delete
-fi
 
 echo -e "PasswordAuthentication no" >> /etc/ssh/sshd_config
 echo 'Port '$ssh_port >> /etc/ssh/sshd_config
 echo $ssh_pub_key >> ~/.ssh/authorized_keys
 /usr/sbin/sshd -D &
 
-exec "$@"
+dockerd
 
 
